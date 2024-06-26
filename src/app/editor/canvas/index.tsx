8dea02e2071d706f41b5d0b49b41bc1de2800571
naman360 <naman.geek@gmail.com>
2024-06-26 23:46:10 +0530

    feat: dragging functionality (wip)

--- src/app/editor/canvas/index.tsx
+++ src/app/editor/canvas/index.tsx
@@ -1,11 +1,14 @@
 "use client";
-import { useEditor } from "@/providers/editor/editor-provider";
+import { EditorElement, useEditor } from "@/providers/editor/editor-provider";
 import React, { useEffect, useRef } from "react";
 
 type Props = {};
 
 const Canvas = (props: Props) => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+  const isDragging = useRef<boolean>(false);
+  const mouseStartingPoints = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
+  const currentShapeIndex = useRef<number>(-1);
   const { state, dispatch } = useEditor();
   useEffect(() => {
     if (canvasRef.current) {
@@ -19,6 +22,10 @@ const Canvas = (props: Props) => {
   }, []);
 
   useEffect(() => {
+    drawImages();
+  }, [state.editor.elements]);
+
+  const drawImages = () => {
     const { elements, canvasCtx } = state.editor;
     if (canvasCtx) {
       canvasCtx.clearRect(
@@ -33,17 +40,103 @@ const Canvas = (props: Props) => {
           const img = new Image();
           img.src = element.content.url;
           img.onload = () => {
-            canvasCtx.drawImage(img, 0, 0, 500, 500);
+            canvasCtx.drawImage(
+              img,
+              element.startX,
+              element.startY,
+              element.width,
+              element.height
+            );
           };
         }
       });
     }
-  }, [state.editor.elements]);
+  };
+  const doesMouseCollides = (
+    mouseX: number,
+    mouseY: number,
+    element: EditorElement
+  ) => {
+    let elementLeft = element.startX;
+    let elementRight = element.startX + element.width;
+    let elementTop = element.startY;
+    let elementBottom = element.startY + element.height;
+
+    if (
+      mouseX > elementLeft &&
+      mouseX < elementRight &&
+      mouseY > elementTop &&
+      mouseY < elementBottom
+    )
+      return true;
+    return false;
+  };
+  const handleMouseDown:
+    | React.MouseEventHandler<HTMLCanvasElement>
+    | undefined = (e) => {
+    e.preventDefault();
+    mouseStartingPoints.current.x = e.clientX - (window.innerWidth - 800); //800 is canvas width
+    mouseStartingPoints.current.y = e.clientY;
+    let index = 0;
+    // Check if mouse click collides with image
+    for (let element of state.editor.elements) {
+      if (
+        doesMouseCollides(
+          mouseStartingPoints.current.x,
+          mouseStartingPoints.current.y,
+          element
+        )
+      ) {
+        console.log("In Image");
+        currentShapeIndex.current = index;
+        isDragging.current = true;
+        return;
+      }
+      index++;
+    }
+  };
+
+  const handleMouseMove:
+    | React.MouseEventHandler<HTMLCanvasElement>
+    | undefined = (e) => {
+    if (!isDragging.current) {
+      return;
+    }
+
+    let mouseX = e.clientX - (window.innerWidth - 800);
+    let mouseY = e.clientY;
+    let dx = mouseX - mouseStartingPoints.current.x;
+    let dy = mouseY - mouseStartingPoints.current.y;
+    let currentShape = state.editor.elements[currentShapeIndex.current];
+    currentShape.startX += dx;
+    currentShape.startY += dy;
+    console.log(mouseX, mouseY, dx, dy, currentShape);
+    dispatch({
+      type: "MODIFY_IMAGE",
+      payload: {
+        index: currentShapeIndex.current,
+        newImage: currentShape,
+      },
+    });
+    // console.log(state.editor.elements);
+    drawImages();
+  };
+
+  const handleMouseUp:
+    | React.MouseEventHandler<HTMLCanvasElement>
+    | undefined = (e) => {
+    e.preventDefault;
+    if (!isDragging.current) return;
+    isDragging.current = false;
+  };
 
   return (
     <canvas
       ref={canvasRef}
       className="border border-black"
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
       id="primary-canvas"
       width={800}
       height={600}
