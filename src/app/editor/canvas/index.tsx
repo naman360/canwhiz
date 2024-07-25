8f12a9a72a7e0bea00371633b9f6ec21ccd7efd4
naman360 <naman.geek@gmail.com>
2024-07-25 15:58:47 +0530

    feat: text functionality implemented on canvas

--- src/app/editor/canvas/index.tsx
+++ src/app/editor/canvas/index.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect, useRef, useState } from "react";
 import { EditorElement, useEditor } from "@/providers/editor/editor-provider";
+import { EditorBtns } from "@/lib/constants";
 
 type Props = {
   activeTool: string;
@@ -12,10 +13,16 @@ const Canvas = (props: Props) => {
   const isDragging = useRef<boolean>(false);
   const mouseStartingPoints = useRef<{ x: number; y: number }>({ x: 0, y: 0 });
   const currentShapeIndex = useRef<number>(-1);
+  const textareaRef = useRef<HTMLTextAreaElement | null>(null);
   const { state, dispatch } = useEditor();
   const [selectedElement, setSelectedElement] = useState<EditorElement | null>(
     null
   );
+
+  const [textAreaPosition, setTextAreaPosition] = useState<{
+    x: number;
+    y: number;
+  } | null>(null);
 
   useEffect(() => {
     if (canvasRef.current) {
@@ -33,14 +40,12 @@ const Canvas = (props: Props) => {
   }, [state.editor.elements]);
 
   const drawOnBgCanvas = () => {
-    const bgCanvas = bgCanvasRef.current;
+    const bgCanvas = canvasRef.current;
     if (!bgCanvas) return;
     const bgCtx = bgCanvas.getContext("2d");
     if (!bgCtx) return;
 
-    bgCtx.clearRect(0, 0, bgCanvas.width, bgCanvas.height);
-    bgCtx.fillStyle = "#ffffff";
-    bgCtx.fillRect(0, 0, bgCanvas.width, bgCanvas.height);
+    // bgCtx.clearRect(0, 0, bgCanvas.width, bgCanvas.height); // TODO: Conflicting with fillText
 
     state.editor.elements.forEach((element) => {
       if (element.type === "image" && element.id !== selectedElement?.id) {
@@ -60,35 +65,81 @@ const Canvas = (props: Props) => {
         img,
         element.startX,
         element.startY,
-        element.width,
-        element.height
+        element.width!,
+        element.height!
       );
     };
   };
 
+  const handleBlur: React.FocusEventHandler<HTMLTextAreaElement> | undefined = (
+    e
+  ) => {
+    const textElementDetails = {
+      type: "text" as EditorBtns,
+      text: e.target.value,
+      startX: textAreaPosition?.x! - (window.innerWidth - 800),
+      startY: textAreaPosition?.y!,
+    };
+
+    const canvasCtx = canvasRef.current?.getContext("2d");
+    if (!canvasCtx) return;
+
+    dispatch({
+      type: "ADD_ELEMENT",
+      payload: textElementDetails,
+    });
+    drawText(textElementDetails);
+    setTextAreaPosition(null);
+  };
+
+  const drawText = (element: {
+    type: EditorBtns;
+    text: string;
+    startX: number;
+    startY: number;
+  }) => {
+    const canvasCtx = canvasRef.current?.getContext("2d");
+    if (!canvasCtx) return;
+    canvasCtx.font = "24px arial";
+    canvasCtx.fillStyle = "#000";
+    canvasCtx.textBaseline = "top";
+    canvasCtx.fillText(element.text, element.startX, element.startY);
+  };
+
   const handleMouseDown: React.MouseEventHandler<HTMLCanvasElement> = (e) => {
+    if (activeTool === "text" && textAreaPosition) return;
+
     e.preventDefault();
-    const mouseX = e.clientX - (window.innerWidth - 800); // Assuming canvas width is 800
+
+    const mouseX = e.clientX - (window.innerWidth - 800); // Assuming canvas width is 800, canvas relative mouseX
     const mouseY = e.clientY;
 
-    let index = 0;
-    for (let element of state.editor.elements) {
-      if (doesMouseCollide(mouseX, mouseY, element)) {
-        currentShapeIndex.current = index;
-        isDragging.current = true;
-        mouseStartingPoints.current = { x: mouseX, y: mouseY };
-        setSelectedElement(element);
+    /** For Text element */
+    if (activeTool === "text") {
+      setTextAreaPosition({ x: e.clientX, y: mouseY });
 
-        dispatch({
-          type: "SET_SELECTED_ELEMENT",
-          payload: {
-            elementId: element.id,
-          },
-        });
+      setTimeout(() => textareaRef.current?.focus(), 0); //textarea display is based on textAreaPosition, initially after setting text position textarearef will be null as ssetstate is async
+    } else {
+      /** For every other element */
+      let index = 0;
+      for (let element of state.editor.elements) {
+        if (doesMouseCollide(mouseX, mouseY, element)) {
+          currentShapeIndex.current = index;
+          isDragging.current = true;
+          mouseStartingPoints.current = { x: mouseX, y: mouseY };
+          setSelectedElement(element);
 
-        break;
+          dispatch({
+            type: "SET_SELECTED_ELEMENT",
+            payload: {
+              elementId: element.id,
+            },
+          });
+
+          break;
+        }
+        index++;
       }
-      index++;
     }
   };
 
@@ -98,9 +149,9 @@ const Canvas = (props: Props) => {
     element: EditorElement
   ) => {
     const elementLeft = element.startX;
-    const elementRight = element.startX + element.width;
+    const elementRight = element.startX + element.width!;
     const elementTop = element.startY;
-    const elementBottom = element.startY + element.height;
+    const elementBottom = element.startY + element.height!;
 
     return (
       mouseX > elementLeft &&
@@ -144,19 +195,20 @@ const Canvas = (props: Props) => {
   };
 
   const handleMouseUp: React.MouseEventHandler<HTMLCanvasElement> = (e) => {
+    if (activeTool === "text" && textAreaPosition) return;
     isDragging.current = false;
     setSelectedElement(null); // Reset selected element state
   };
 
   return (
     <div className="flex flex-col relative">
-      <canvas
+      {/* <canvas
         ref={bgCanvasRef}
         className="border border-black absolute"
         id="bg-canvas"
         width={800}
         height={600}
-      />
+      /> */}
       <canvas
         ref={canvasRef}
         className="border border-black z-1"
@@ -167,13 +219,15 @@ const Canvas = (props: Props) => {
         onMouseMove={handleMouseMove}
         onMouseUp={handleMouseUp}
       />
-      {activeTool === "text" ? (
+      {activeTool === "text" && textAreaPosition ? (
         <textarea
+          ref={textareaRef}
           className="writing_area"
+          onBlur={handleBlur}
           style={{
-            top: 0,
-            left: 0,
-            font: `24 px sans-serif`,
+            top: textAreaPosition.y, // *textAreaPosition state is relative to screen, hence taking mouse coordinates
+            left: textAreaPosition.x,
+            font: `24px arial`,
           }}
         />
       ) : null}
